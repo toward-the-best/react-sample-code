56517ba9076bf3ba77d89df1d71b79d739f93629
toward-the-best <towardthebestprogrammer@gmail.com>
2023-05-14 14:52:19 +0900

    예제 수정

--- js-test-code/fetchData.js
+++ js-test-code/fetchData.js
@@ -1,4 +1,6 @@
 async function getData() {
+    // fetch 내장 함수를 사용하여 API 정보를 가져올수 있다.
+    // return 값은 promise 형식 입니다.
     let rawResponse = await fetch("https://jsonplaceholder.typicode.com/posts");
     let jsonResponse = await rawResponse.json();
     console.log(jsonResponse);
